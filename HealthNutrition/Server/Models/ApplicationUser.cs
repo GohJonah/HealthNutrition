ee1db6f6c9ab11df0b0c0132a6c07c4acc719618
GOH JONAH 2204785F <2204785F@student.tp.edu.sg>
2023-12-10 15:46:30 +0800

    Add project files.

---
+++ HealthNutrition/Server/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using HealthNutrition.Client;
+using Humanizer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Migrations;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using System.Text;
+using System.Threading.Channels;
+
+namespace HealthNutrition.Server.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+    }
+}
