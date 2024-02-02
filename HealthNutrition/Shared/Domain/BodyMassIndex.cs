99d77c810e084416427952cb1512b7740bd13ee0
GOH JONAH 2204785F <2204785F@student.tp.edu.sg>
2024-02-03 00:59:37 +0800

    Final Full Stack Project Code

--- HealthNutrition/Shared/Domain/BodyMassIndex.cs
+++ HealthNutrition/Shared/Domain/BodyMassIndex.cs
@@ -11,8 +11,10 @@ namespace HealthNutrition.Shared.Domain
     public class BodyMassIndex : BaseDomainModel
     {
         [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Weight must be greater than 0")]
         public Double? Weight { get; set; }
         [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Height must be greater than 0")]
         public Double? Height { get; set; }
         public Double? BMI { get; set; }
 
